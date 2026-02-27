4f727c0dd95492621b5fd1922384ae4fb2b040fd
kanishksingh23 <kanishksingh763@gmail.com>
2026-02-27 10:09:06 +0000

    fix: add Ctrl+Z suspend binding to exit TUI and return to terminal

--- demo_creator/app.py
+++ demo_creator/app.py
@@ -1,4 +1,5 @@
 from textual.app import App
+from textual.binding import Binding
 from demo_creator.screens import (
     LoginScreen,
     MainMenuScreen,
@@ -9,6 +10,10 @@ from demo_creator.screens import (
 
 class DemoCreatorApp(App):
     CSS_PATH = "./assets/base.tcss"
+
+    BINDINGS = [ 
+       Binding("ctrl+z", "suspend_process", "Suspend (go to terminal)", show=True), 
+    ]
 
     def on_mount(self):
         self.push_screen(LoginScreen())
